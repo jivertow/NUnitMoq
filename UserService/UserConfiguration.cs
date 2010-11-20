9ec86afd9d2b920c9491f319b862369fab965ec7
Jan <jan@ivertowski.de>
2010-11-20 17:44:41 +0100

    git add.gitignore git add	Tests.nunit initial commit

---
+++ UserService/UserConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MdP.Interfaces;
+
+namespace MdP
+{
+    public class UserConfiguration : IUserConfiguration
+    {
+        public string GetUserInformation(int userId)
+        {
+            if (userId == 123) return "Hans Wurst";
+            else return "";            
+        }
+    }
+}
---
+++ UserServiceTest/UserConfigurationTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using MdP.Interfaces;
+using MdP;
+using Moq;
+
+namespace UserServiceTest
+{
+    [TestFixture]
+    public class FunctionTests
+    {
+        [SetUp]
+        public void Init()
+        {
+            
+        }
+        [Test]
+        public void GetUser123Test()
+        {
+            IUserConfiguration userConf = new UserConfiguration();
+            string result = userConf.GetUserInformation(123);
+
+            Assert.That(result, Is.EqualTo("Hans Wurst"));
+        }
+
+        [Test]
+        public void GetEmptyUserTest()
+        {
+            IUserConfiguration userconf = new UserConfiguration();
+            string result = userconf.GetUserInformation(1);
+
+            Assert.That(result, Is.EqualTo(""));
+        }
+
+        [Test]
+        public void GetUser123MockTest()
+        {
+            var mock = new Mock<IUserConfiguration>();
+            mock.Setup(
+                e => e.GetUserInformation(123)
+                ).Returns("Hans Wurst");
+
+            Assert.That(mock.Object.GetUserInformation(123), Is.EqualTo("Hans Wurst"));
+        }
+    }
+}
